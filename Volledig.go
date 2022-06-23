3205b17ecab9353bda89322ce348027a919e749f
Krappiekop <82145883+Krappiekop@users.noreply.github.com>
2022-06-23 19:21:40 +0200

    Update Volledig.go

--- Volledig.go
+++ Volledig.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -10,6 +11,7 @@ import (
 )
 
 var total, cpuUserPerc, cpuSysPerc, cpuIdlePerc float64
+var location string
 
 func cpu_shit() {
 	cpu, err := cpu.Get() //hier worden de cpu gegevens opgehaald
@@ -21,10 +23,9 @@ func cpu_shit() {
 	cpuUserPerc = float64(cpu.User) / total * 100
 	cpuSysPerc = float64(cpu.System) / total * 100
 	cpuIdlePerc = float64(cpu.Idle) / total * 100
-
 }
 
-func write_to_file(file *File) {
+func write_to_file() {
 
 	cpuUserAfgerond := fmt.Sprintf("%.2f", cpuUserPerc) //hier geef ik aan dat er maar 2 decimalen achter de komma komen
 	cpuSysAfgerond := fmt.Sprintf("%.2f", cpuSysPerc)
@@ -32,6 +33,10 @@ func write_to_file(file *File) {
 
 	//de rest dat nog moet :)
 
+	file, err := os.OpenFile(location, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644) //hier wordt een logbestand aangemaakt, //handle de error :)
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
 	Tijd := time.Now()
 	file.WriteString(Tijd.Format("01-02-2006 15:04:05") + "\n") //hier wordt de datum en tijd geprint
 	file.WriteString("cpu user: " + cpuUserAfgerond + " %\n")   //hier wordt de cpu geprint
@@ -40,19 +45,25 @@ func write_to_file(file *File) {
 }
 
 func main() {
-	//vraag de user om een locatie, mag jij doen e.g: ./log.txt :)
-	location := "/Log.txt"
-	//vraag de user om een tijd, zie https://www.geeksforgeeks.org/time-parse-function-in-golang-with-examples/ voor parsing :)
-	tijd := time.Second * 5
+	scanner := bufio.NewScanner(os.Stdin)
 
-	file, err := os.Create(location) //hier wordt een logbestand aangemaakt, //handle de error :)
+	//vraag de user om een locatie, mag jij doen e.g: ./log.txt :)
+	//todo: primt bericht zoals vul de log locatie in...
+	fmt.Println("Vul hier de log locatie in:")
+	scanner.Scan()
+	location = scanner.Text()
+	//vraag de user om een tijd:) met bericht zoals vul een interval zoals 5s, 30m in
+	//boven de scanner.scan
+	fmt.Println("Vul hier de frequentie in:")
+	scanner.Scan()
+	tijd, err := time.ParseDuration(scanner.Text())
 	if err != nil {
-		log.Fatalf("failed creating file: %s", err)
+		fmt.Println(err)
 	}
+
 	for {
 		time.Sleep(tijd) //maak dit ook configureerbaar door te vragen
-
 		cpu_shit()
-		write_to_file(file)
+		write_to_file()
 	}
 }
